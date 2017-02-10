78ab57e285f1a08040c266e870e3e855caa87e45
Elysium19921 <yaroslav.ponomarenko.1992@mail.ru>
2017-02-10 21:01:32 +0200

    collections

--- UnitTestCalculator/UnitTestOutputer.cs
+++ UnitTestCalculator/UnitTestOutputer.cs
@@ -11,38 +11,61 @@ namespace UnitTestCalculator
     [TestClass]
     public class UnitTestOutputer
     {
+
         [TestMethod]
-
-
-        public void TestOutputer()
+        public void TestCollectionMyelementExist()
         {
-            // output prompt: a
 
-            // public string output_double(string prompt, double a)
+            // Object type of all data
 
-            // arrange
-            string my_text = "Hello";
-            double my_number = 7;
+            List<MyElement> list_of_myelements = new List<MyElement>();
 
-            string expected_result = "Hello: 7";
+            MyElement elm_1 = new MyElement();
+            MyElement elm_2 = new MyElement();
+            MyElement elm_3 = new MyElement();
 
-            // act
+            elm_2 = null; // lose pointer on data element 2
 
-            Outputer outputer = new Outputer();
+            list_of_myelements.Add(elm_1);
+            list_of_myelements.Add(elm_2);
+            list_of_myelements.Add(elm_3);
 
-            string result = outputer.output_double(my_text, my_number);
 
 
-            // assert for text with register
-            //Assert.AreEqual(expected_result, result, false);
+            // object.GetType() return type of element
 
-            //with out register
-            Assert.AreEqual("HI", result, false);
-
-            // result and expected result in one memory place
-
-            Assert.AreSame(expected_result, result);
+            CollectionAssert.AllItemsAreNotNull(list_of_myelements);
 
         }
+
+
+
+        [TestMethod]
+        public void TestCollectionEqualToOtherOrder()
+        {
+            List<string> list_of_string_test = new List<string>();
+
+            list_of_string_test.Add("Abrasha");
+            list_of_string_test.Add("Ivanov");
+            list_of_string_test.Add("Petrov");
+
+
+            List<string> list_of_string_expected = new List<string>();
+
+            list_of_string_expected.Add("Abrasha");
+            list_of_string_expected.Add("Ivanov");
+            list_of_string_expected.Add("Petrov");
+
+
+
+
+            CollectionAssert.AreEqual(list_of_string_expected, list_of_string_test);
+        }
+
+
+
     }
 }
+
+
+
